bb4a9130b01b7f017ba0220c45db1ad2e96ec205
Z1-FS <user@Users-MacBook-Pro-2.local>
2022-06-28 15:40:07 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:z1_web_view_app/firebase_options.dart';
 import 'configs/auto_route/auto_route.gr.dart';
-void main() {
-  runApp( MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
-      final _appRouter = AppRouter(); 
+  final _appRouter = AppRouter();
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
   @override
   Widget build(BuildContext context) {
-    
     return GetMaterialApp.router(
-     // theme: theme(),
+      // theme: theme(),
       debugShowCheckedModeBanner: false,
       routerDelegate: _appRouter.delegate(),
       routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
-
